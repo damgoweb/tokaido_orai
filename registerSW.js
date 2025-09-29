303d2b72f3a2201784c7fe281761af785e10bcb3
damgoweb <dannojo.engineer@gmail.com>
2025-09-30 00:34:38 +0800

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/tokaido-app/sw.js', { scope: '/tokaido-app/' })})}
